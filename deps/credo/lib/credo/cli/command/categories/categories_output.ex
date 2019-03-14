f8888590bb80bde872e0044e6f863edd201bbf83
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-03-14 18:08:39 +0200

    remove gitignored

--- deps/credo/lib/credo/cli/command/categories/categories_output.ex
+++
@@ -1,9 +0,0 @@
-defmodule Credo.CLI.Command.Categories.CategoriesOutput do
-  @moduledoc false
-
-  alias Credo.CLI.Command.Categories.Output.Default
-
-  def print_categories(_exec) do
-    Default.print_categories()
-  end
-end
